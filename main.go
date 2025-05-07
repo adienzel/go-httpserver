104e896db46dbbb2677515651380e4874bc7e8c1
Adi Enzel <adienzel@gmail.com>
2025-05-07 18:03:20 -0400

    print

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/valyala/fasthttp"
+	"log"
 	"math/rand"
 	"strconv"
 	"time"
@@ -24,17 +26,25 @@ func generateRandomAlphanumeric(length int) []byte {
 func main() {
 	requestHandler := func(ctx *fasthttp.RequestCtx) {
 		nano := time.Now().UnixNano()
+		var str string
 		body := generateRandomAlphanumeric(rand.Intn((MaxBody - MinBody + 1) + MinBody))
-		if str := ctx.Request.Header.Peek("X-Arrived-time"); str != nil {
+		if str = ctx.Request.Header.Peek("X-Arrived-time"); str != nil {
 			ctx.Response.Header.Set("X-Arrived-time", string(str))
 		}
-		if str := ctx.Request.Header.Peek("X-Start-Time"); str != nil {
+		if str = ctx.Request.Header.Peek("X-Start-Time"); str != nil {
 			ctx.Response.Header.Set("X-Start-Time", string(str))
 		}
 		ctx.Response.Header.Set("X-App-time", strconv.FormatInt(nano, 10))
 		ctx.Response.Header.Set("Content-Type", "application/text")
 		ctx.Response.Header.Set("Server", "OUR.TEST.SERVER/28")
 		ctx.SetBodyString(string(body))
+		startTime, err := strconv.ParseInt(str, 10, 64) // base 10, 64-bit
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		log.Printf("take %d", (nano-startTime)/1000000)
 	}
 
 	if err := fasthttp.ListenAndServe(":8080", requestHandler); err != nil {
